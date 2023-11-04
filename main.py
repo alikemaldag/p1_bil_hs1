0385347d62ccfdcb6975745c2516fcf24374aa66
alikemaldag <149866729+alikemaldag@users.noreply.github.com>
2023-11-04 10:04:11 +0000

    Hepsi denendi

--- main.py
+++ main.py
@@ -1,1 +1,82 @@
-print('Merhaba Dünya')
+#print("Fonksiyon şu şekilde tanımlanır: print('ekrana yazdırılacak yazı')")
+
+#print("Merhaba Dünya\nMerhaba Dünya\nMerhaba Dünya\n")
+
+#print("Merhaba"+" "+"Şahin")
+
+#print(input("Adınız Nedir? "))
+
+#print("Merhaba " + input("Adınız Nedir? "))
+
+#print(len(input("Adınız Nedir? ")))
+
+#name = input("Adınız Nedir? ")
+#print(name)
+
+#name = "ali kemal"
+#print(name)
+#name = "ali"
+#print(name)
+
+#print(len(input("Adınız Nedir? ")))
+#/
+#isim = input("Adın Nedir?" )
+#uzunluk = len(isim)
+#print(uzunluk)
+
+#sayi1 = input("1. sayıyı giriniz: ")
+#sayi2 = input("2. sayıyı giriniz: ")
+#sayi3 = sayi1
+#sayi1 = sayi2
+#sayi2 = sayi3
+#print("Sayı 1: " + sayi1)
+#print("Sayı 2: " + sayi2)
+
+#PROJELER
+
+#enSonGidilenSehirAdi = input("Son gittiğiniz şehir? ")
+#evcilHayvanAdi = input("Hayvanınızın adı nedir? ")
+#print("Müzik Grup Adı: " + enSonGidilenSehirAdi + " " +evcilHayvanAdi)
+
+#sayi1 = input("1. sayıyı giriniz: ")
+#sayi2 = input("2. sayıyı giriniz: ")
+#toplam = int(sayi1) + int(sayi2)
+#print("Toplam = " + str(toplam))
+
+#kisakenar = input("Kısa kenar uzunluğu kaçtır? ")
+#uzunkenar = input("Uzun kenar uzunluğu kaçtır? ")
+#cevre = (int(kisakenar) + int(uzunkenar)) * 2
+#alan = int(kisakenar) * int(uzunkenar)   
+#print("Çevresi: " + str(cevre))
+#print("Alanı: " + str(alan))
+
+#TAMSAYI DEĞERLER
+#yaricap = int(input("Yarıçapı giriniz: "))
+#yukseklik = int(input("Yükseklik giriniz: "))
+#pi = 3.14
+#tabanalani = pi * (yaricap) * (yaricap)
+#tabancevresi = 2 * pi * (yaricap)
+#yanalalan = (yukseklik) * (tabancevresi)
+#toplamalan = (yanalalan) + (2 * tabanalani)
+#hacim = (tabanalani) * (yukseklik)
+#print("Silindirin hacmi: " + str(hacim))
+#print("Silindirin Alanı: " + str(toplamalan)) 
+
+#KÜSÜRLÜ DEĞERLER
+# yaricap = float(input("Yarıçapı giriniz: "))
+# yukseklik = float(input("Yüksekliği giriniz: "))
+# pisayisi = 3.14
+# tabanAlani = (pisayisi) * (yaricap) * (yaricap)
+# tabanCevresi = 2 * (pisayisi) * (yaricap)
+# yanalAlan = (yukseklik) * (tabanCevresi)
+# toplamAlan = (yanalAlan) + ((2 * tabanAlani))
+# hacim = (tabanAlani) * (yukseklik) 
+# print("Silindirin Hacmi: " + str(hacim))
+# print("Silindirin Alanı: " + str(toplamAlan))
+
+#yaricap = float(input("Yarıçapı giriniz: "))
+#pisayisi = 3.14
+#hacim = pisayisi * yaricap * yaricap
+#cevre = 2 * pisayisi * yaricap
+#print("Hacmi: " + str(hacim))
+#print("Çevresi: " + str(cevre))
